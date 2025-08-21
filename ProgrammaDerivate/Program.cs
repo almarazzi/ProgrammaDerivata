7447ceb9a4e7a726eb27f652f3eae46f4a6a3cca
almar <almar@PC_ALBY>
2025-08-21 16:25:30 +0200

    Rifattorizzazione delle istruzioni per le derivate
    Rimosse istruzioni statiche per il calcolo delle derivate di funzioni specifiche.
    Introdotto un ciclo che utilizza un array di segni matematici per generare dinamicamente le istruzioni, rendendo il codice più conciso e flessibile.

--- ProgrammaDerivate/Program.cs
+++ ProgrammaDerivate/Program.cs
@@ -4,12 +4,16 @@ using ProgrammaDerivate;
 
 Console.WriteLine("Qui vengono calcolate le derivate prime.");
 Console.WriteLine("Per calcolare la derivata di una potenza, usa una notazione come y=(x^9).");
-Console.WriteLine("Per calcolare la derivata k/f(x), usa una notazione come y=(k/f(x)).");
-Console.WriteLine("Per calcolare la derivata k*f(x), usa una notazione come y=(k/f(x)).");
-Console.WriteLine("Per calcolare la derivata f(x)+g(x), usa una notazione come y=((f(x))+(g(x))).");
-Console.WriteLine("Per calcolare la derivata f(x)-g(x), usa una notazione come y=((f(x))-(g(x))).");
-Console.WriteLine("Per calcolare la derivata f(x)*g(x), usa una notazione come y=((f(x))*(g(x))).");
-Console.WriteLine("Per calcolare la derivata f(x)/g(x), usa una notazione come y=((f(x))/(g(x))).");
+char[] Segni = { '+', '-', '/', '*' };
+for (int i = Segni.Length-1; i > 1; i--)
+{
+    Console.WriteLine($"Per calcolare la derivata k{Segni[i]}f(x), usa una notazione come y=(k{Segni[i]}f(x)).");
+}
+for (int i = 0; i < Segni.Length; i++)
+{
+    Console.WriteLine($"Per calcolare la derivata f(x){Segni[i]}g(x), usa una notazione come y=((f(x)){Segni[i]}(g(x))).");
+
+}
 Console.WriteLine("inserire la tua funzione qui sotto.");
 var ascolto = new Ascolto();
 var f = Console.ReadLine();
